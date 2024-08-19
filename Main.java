49b47f3b24c41875f718a357ed906668c52fa529
Lailson96 <lailsonrd@gmail.com>
2024-08-18 22:52:03 -0300

    ultimo1808

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Locadora");
-        System.out.println("Filme: Top Gun");
-
+        int ano = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        double media = (8.1 + 7.8 + 2.5) / 3;
+        String mensagem = """
+                Locadora
+                Filme: Top Gun
+                Ano de lançamento: %d
+                Nota: %.2f
+                """.formatted(ano, media);
+        System.out.println(mensagem);
     }
 }
